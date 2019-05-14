7bd7316a5a96cb5108521a71159d5e95e0ab84f7
LeiShifu <wangyingjia1024@gmail.com>
2019-05-14 18:59:12 +0800

    libraries_for_slave_board

---
+++ hx711.h
@@ -0,0 +1,12 @@
+#ifndef __HX711__H__
+#define __HX711__H__
+
+#include <Arduino.h>
+
+extern void Init_Hx711();
+extern void calibrate();
+
+extern unsigned long Hx711_Read(void);
+extern long Get_Force(void);
+
+#endif
